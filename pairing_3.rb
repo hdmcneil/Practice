2e16bb8a76ea0f2f7364c03ce42543780327cd31
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-10 12:15:38 -0700

    Update file

--- pairing_3.rb
+++ pairing_3.rb
@@ -34,15 +34,47 @@
 # p person2
 
 #Exercise 2: Iteration
-friends = [1, 50, 99, 43, 15, 78, 65, 9]
+# friends = [1, 50, 99, 43, 15, 78, 65, 9]
 
-friends.each do |friend|
-    if friend > 30 
-        puts friend
-    end
-end
-friends.each do |friend|
-    if friend < 60 
-        puts friend  
-    end
-end
+# friends.each do |friend|
+#     if friend > 30 
+#         puts friend
+#     end
+# end
+# friends.each do |friend|
+#     if friend < 60 
+#         puts friend  
+#     end
+# end
+#
+# name = ["heather", "anna"]
+
+# name.each do |capname|
+
+#     puts capname.capitalize
+# end
+
+# words = ["battle", "little", "tile", "great"]
+
+# words.each do |word|
+#     p word.gsub("t", "T")
+# end
+
+# words = ["battle", "little", "tile", "great"]
+
+# words.each do |word|
+#     p word.tr("t", "T")
+# end
+
+# names = ["Heather", "Ana", "Ty", "Frank", "Bet", "Brad", "Xander", "Sooyung"]
+
+# names.sort!
+# list = names.each_index
+# list.each {|index| puts "#{index+1}. #{names[index]}"}
+    
+# cell = [5, 0, 3, 9, 9, 8, 9, 5, 2, 6]
+
+# puts cell.insert(3, :-).insert(7, :-).join
+
+
+
